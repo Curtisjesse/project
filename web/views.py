8c67b1279017b3472240d82c34eac7bbd35303c3
curtisjesse <curtisjesse487@gmail.com>
2024-02-28 13:05:51 +0300

    appointment

--- web/views.py
+++ web/views.py
@@ -4,4 +4,29 @@ def index(request):
     
     return render(request, "index.html")
 
+def blog(request):
+    
+    return render(request, "blog.html")
+
+def contact(request):
+    
+    return render(request, "contact.html")
+
+def departments(request):
+    
+    return render(request, "departments.html")
+
+def about(request):
+    
+    return render(request, "about.html")
+
+def doctor(request):
+    
+    return render(request, "doctor.html")
+
+def appointment(request):
+    
+    return render(request, "appointment.html")
+
+
 # Create your views here.
