b7c73ae1d9596da3bec332276bd50b2df726651d
curtisjesse <curtisjesse487@gmail.com>
2024-03-01 12:54:00 +0300

    socialmedia

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Doctors
+from .models import Doctors,Socialmedia
 # Register your models here.
 admin.site.register(Doctors)
+admin.site.register(Socialmedia)
--- web/models.py
+++ web/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Socialmedia(models.Model):
+    facebooklink = models.URLField(blank=True, null=True)
+    twitterlink = models.URLField(blank=True, null=True)
+    instagramlink = models.URLField(blank=True, null=True)
+    
+    
 class Doctors(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     specialized = models.TextField()
     img = models.ImageField(upload_to = 'images')
+    Socialmedia = models.ForeignKey(Socialmedia,related_name='socialmedia',on_delete=models.SET_NULL,null=True,blank=True)
+    
     
     def __str__(self):
         return self.name
